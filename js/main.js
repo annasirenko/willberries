01a29646b6104cdd2d8dd783cfef45907034e6b4
Anna <otherjam@gmail.com>
2021-02-04 21:16:19 +0200

    finished markup and slider

---
+++ js/main.js
@@ -0,0 +1,15 @@
+const swiper = new Swiper('.swiper-container', {
+  // Optional parameters
+ 
+  loop: true,
+
+ 
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.slider__button--next',
+    prevEl: '.slider__button--prev',
+  },
+
+ 
+});
